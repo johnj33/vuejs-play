b9e63dd467322dfd72d2486d07bb122a570645cf
John Jackson <john.jackson@advancedcomputersoftware.com>
2016-09-16 13:49:22 +0100

    initial commit

---
+++ src/components/JohnsComponent/JohnsComponent.vue
@@ -0,0 +1,10 @@
+<script src="./johnsComponent.js"></script>
+
+<template>
+    <div class="JohnsComponent">
+        <button v-on:click="buttonPressed">{{* message }}</button>
+        <div>{{message}}</div>
+    </div>
+</template>
+
+
---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import App from './App'
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#main',
+  components: { App }
+})
+
+new Vue({
+  el: '#second',
+  components: { App }
+})
